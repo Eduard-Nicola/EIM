2eb5a41c31ade38cb162f4add58e75ccd43da474
Eduard-Nicola <nicola.mihai.eduard@gmail.com>
2020-02-21 09:16:27 +0200

    Implemented functionality for customized message, rotate screen and fade effect

--- labtasks/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
+++ labtasks/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package ro.pub.cs.systems.eim.lab01.myfirstandroidapplication;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,8 +27,18 @@ public class MainActivity extends AppCompatActivity {
             greetingTextView.setAlpha(1);
 
             // TODO: exercise 9a
-
+            greetingTextView.setText(greetingTextView.getText().toString().replace("xxx", "\n"+userNameEditText.getText()));
             // TODO: exercise 9b
+            AlphaAnimation fadeEffect = new AlphaAnimation(1.0f, 0.0f);
+            fadeEffect.setDuration(TRANSPARENCY_EFFECT_DURATION);
+            fadeEffect.setFillAfter(true);
+            greetingTextView.setAnimation(fadeEffect);
+            // TODO: bonus fake
+//            SensorManager sensorManager;
+//            Sensor sensor;
+//
+//            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         }
 
     }
@@ -35,5 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button submitButton = (Button)findViewById(R.id.submit_button);
         submitButton.setOnClickListener(buttonClickListener);
+
+        // TODO: bonus
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
